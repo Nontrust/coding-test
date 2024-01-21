776014e1573e6ea35e5c83557161c05d2add3232
Nontrust <=>
2024-01-21 22:38:18 +0900

    [3_시뮬레이션]  문제풀이 2

--- .idea/inflearn/3_simulation/2. 청소 로봇 1.py
+++ .idea/inflearn/3_simulation/2. 청소 로봇 1.py
@@ -1,6 +1,15 @@
+control = {
+    'R': [0,1],
+    'L': [0,-1],
+    'U': [-1,0],
+    'D': [1,0],
+}
 def solution(moves):
     x = y = 0
-    
+    for s in moves:
+        x += control[s][0]
+        y += control[s][1]
+
     return [x, y]
                             
 print(solution('RRRDDDLU'))
