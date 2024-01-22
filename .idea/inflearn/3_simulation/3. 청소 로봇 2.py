33058536e6d7433c595995f7e18b7a27aa1ae2eb
Nontrust <=>
2024-01-22 21:13:54 +0900

    [3_시뮬레이션]  문제풀이 3

--- .idea/inflearn/3_simulation/3. 청소 로봇 2.py
+++ .idea/inflearn/3_simulation/3. 청소 로봇 2.py
@@ -1,8 +1,20 @@
-def solution(n, moves): 
+control = {
+    'R': [0,1],
+    'L': [0,-1],
+    'U': [-1,0],
+    'D': [1,0],
+}
+def solution(n, moves):
     x = y = 0
-    
- 
-    return [x, y]    
+    for s in moves:
+        tx = x + control[s][0]
+        ty = y + control[s][1]
+        if tx in range(6) and ty in range(6):
+            x=tx
+            y=ty
+
+
+    return [x, y]
                 
                       
 print(solution(5, 'RRRDDDUUUUUUL'))
