4d245594a1da3f5360ba92a7c6d78abe0f417653
Nontrust <deed1515@naver.com>
2024-01-28 18:13:41 +0900

    [3_시뮬레이션]  문제풀이 3

--- .idea/inflearn/3_simulation/5. 위험 지역.py
+++ .idea/inflearn/3_simulation/5. 위험 지역.py
@@ -1,7 +1,17 @@
+controls = [[-1,-1], [0,-1], [1,-1],
+            [-1,0], [1,0],
+            [-1,1], [0,1], [1,1]]
 def solution(board):
-    answer = 0
-   
-    
+    answer, n = 0, len(board)
+    for x in range(n):
+        for y in range(n):
+            if board[x][y] == 1:
+                for i in controls:
+                    nx, ny = x+i[0], y+i[1]
+                    if nx>=0 and nx<n and ny>=0 and ny<n and board[nx][ny] == 0:
+                        board[nx][ny] = -1
+                        answer += 1
+
     return answer
                        
 print(solution([[0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]))
