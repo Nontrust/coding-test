1b480fac5119b0af4cd0430517bc0664037d05b7
Nontrust <deed1515@naver.com>
2024-01-30 20:06:00 +0900

    [4_정렬]  문제풀이 3 - two pointer

--- .idea/inflearn/4_sort/3. 두 수의 합(O(nlogn)).py
+++ .idea/inflearn/4_sort/3. 두 수의 합(O(nlogn)).py
@@ -1,10 +1,20 @@
 def solution(nums, target):
-    answer = [0]*2
-   
-            
+    answer = [0] * 2
+    rt, lt = 0, len(nums) - 1
+    nums.sort()
+    while rt<lt:
+        sum = nums[lt] + nums[rt]
+        if sum == target:
+            break
+        elif sum < target:
+            rt += 1
+        else:
+            lt -= 1
+        if rt >= lt:
+            return [0, 0]
+        answer = [nums[rt], nums[lt]]
     return answer
-    
-                                        
+
 print(solution([3, 7, 2, 12, 9, 15, 8, 11], 12))
 print(solution([21, 12, 30, 15, 6, 2, 9, 19, 14], 24))
 print(solution([12, 18, 5, 8, 21, 27, 22, 25, 16, 2], 28))
