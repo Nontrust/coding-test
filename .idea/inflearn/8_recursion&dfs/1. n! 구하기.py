eadf2abb9e5e18a99b4f617a0e2f6d21df2efe62
P173073 <deed1515@naver.com>
2024-07-04 18:30:07 +0900

    [8_recursion & dfs] 재귀

--- .idea/inflearn/8_recursion&dfs/1. n! 구하기.py
+++ .idea/inflearn/8_recursion&dfs/1. n! 구하기.py
@@ -1,5 +1,11 @@
 def DFS(n):
-    
+    result = 1
+    if n == 1:
+        return result
+    else:
+        return n * DFS(n-1)
 
 print(DFS(5))             
 print(DFS(6))
+print(DFS(7))
+print(DFS(8))
--- .idea/inflearn/8_recursion&dfs/2. 피보나치 수열.py
+++ .idea/inflearn/8_recursion&dfs/2. 피보나치 수열.py
@@ -1,5 +1,14 @@
 def DFS(n):
-    
+    if n == 1 or n ==2:
+        return 1
+    else:
+        return DFS(n-1) + DFS(n-2)
+
+
               
 print(DFS(5))
+print(DFS(6))
+print(DFS(7))
+print(DFS(8))
+print(DFS(9))
 
