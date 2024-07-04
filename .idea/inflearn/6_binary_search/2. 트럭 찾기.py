e972efe16c5f7e8f600074c59d7cb4f7432856fb
P173073 <deed1515@naver.com>
2024-07-04 16:36:42 +0900

    [6_binary search] 이진탐색 활용

--- .idea/inflearn/6_binary_search/2. 트럭 찾기.py
+++ .idea/inflearn/6_binary_search/2. 트럭 찾기.py
@@ -1,12 +1,27 @@
-def solution(nums, weight):
-    answer = 0
-    
+# from bisect import bisect_left
+#
+# def solution(nums, weight):
+#     answer = bisect_left(nums, weight)
+#     return answer if answer == len(nums) else -1
 
-    return answer
+
+def solution2(nums, weight):
+    lt, rt = 0, len(nums)
+    nums = sorted(nums)
+    while rt > lt:
+        mid = (rt + lt) //2
+        if(nums[mid] < weight):
+            lt = mid +1
+        else:
+            rt = mid
+
+
+    return -1 if rt == len(nums) else rt
+
     
      
-print(solution([100, 120, 150, 160, 165, 170, 175, 180, 190, 200], 170))                                          
-print(solution([200, 250, 260, 265, 275, 290, 300, 325, 350, 370], 270))
-print(solution([50, 55, 60, 65, 70, 80, 80, 80, 80, 90, 90, 90], 80))
-print(solution([20, 30, 40, 50, 60, 70], 90))
-print(solution([10, 30, 50, 70, 80, 90, 100, 110, 120], 115))
+print(solution2([100, 120, 150, 160, 165, 170, 175, 180, 190, 200], 170))
+print(solution2([200, 250, 260, 265, 275, 290, 300, 325, 350, 370], 270))
+print(solution2([50, 55, 60, 65, 70, 80, 80, 80, 80, 90, 90, 90], 80))
+print(solution2([20, 30, 40, 50, 60, 70], 90))
+print(solution2([10, 30, 50, 70, 80, 90, 100, 110, 120], 115))
--- .idea/inflearn/6_binary_search/3. 고정된 숫자.py
+++ .idea/inflearn/6_binary_search/3. 고정된 숫자.py
@@ -1,9 +1,23 @@
+def solution2(nums):
+    answer = -1
+    for i, num in enumerate(nums):
+        if (num == i):
+            return i
+    
+    return answer
+
 def solution(nums):
-    answer = 0
-    
-    
-    return answer    
-     
+    lt, rt = 0, len(nums) -1
+    while rt >= lt:
+        mid = (rt+lt)//2
+        if(nums[mid] == mid):
+            return nums[mid]
+        elif(nums[mid] < mid):
+            lt = mid +1
+        else:
+            rt = mid - 1
+
+    return -1
     
 print(solution([-3, -2, 0, 1, 3, 5, 8, 9, 12]))
 print(solution([-10, -5, -2, 3, 4, 6, 7, 8]))
