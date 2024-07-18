4f1bc8e9ae4e1838f7b87ffc5d60c0011ac6d037
P173073 <deed1515@naver.com>
2024-07-18 09:46:05 +0900

    [8_recursion & dfs] dfs

--- .idea/inflearn/8_recursion&dfs/3. 검정색 영역구하기.py
+++ .idea/inflearn/8_recursion&dfs/3. 검정색 영역구하기.py
@@ -1,8 +1,25 @@
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 def solution(board):
     answer = 0
-    
+    for x in range(5):
+        for y in range(5):
+            if board[x][y]  == 1:
+                answer += 1
+                DFS(x,y,board)
+
     
     return answer
+
+def DFS(x, y, board):
+    board[x][y] =0
+    for k in range(4):
+        nx = x+dx[k]
+        ny = y+dy[k]
+        if(nx >= 0 and nx <5 and ny >= 0 and ny < 5 and board[nx][ny]):
+            DFS(nx,ny,board)
+
+
             
 print(solution([[0, 1, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
 print(solution([[1, 1, 1, 0, 1], [1, 1, 1, 0, 1], [0, 0, 1, 0, 0], [1, 1, 0, 1, 0], [1, 0, 1, 0, 0]]))
--- .idea/inflearn/8_recursion&dfs/4. 픽셀수 구하기.py
+++ .idea/inflearn/8_recursion&dfs/4. 픽셀수 구하기.py
@@ -1,9 +1,31 @@
+mx = [1,0,-1,0]
+my = [0,1,0,-1]
+cnt = 0
+
 def solution(board):
+    global cnt
     answer = []
-    
-    
+    for x in range(5):
+        for y in range(5):
+            if board[x][y] == 1:
+                cnt = 0
+                DFS(x, y, board)
+                answer.append(cnt)
     return answer
-            
+
+def DFS(x,y,board):
+    global cnt
+    board[x][y] = 0
+    cnt = cnt + 1
+    for k in range(4):
+        dx = mx[k] + x
+        dy = my[k] + y
+        if dx>=0 and dy>=0 and dx<5 and dy<5 and board[dx][dy] == 1:
+            DFS(dx,dy,board)
+# 5,3
+# 7,
+
+
 print(solution([[0, 1, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
 print(solution([[1, 1, 1, 0, 1], [1, 1, 1, 0, 1], [0, 0, 1, 0, 0], [1, 1, 0, 1, 0], [1, 0, 1, 0, 0]]))
 print(solution([[0, 0, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [1, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
