6dd22a5bd48d16fe090f3209887f87ce484f0026
P173073 <deed1515@naver.com>
2024-07-23 09:31:20 +0900

    [9_bfs]

--- .idea/inflearn/9_bfs/2. 최소 점프.py
+++ .idea/inflearn/9_bfs/2. 최소 점프.py
@@ -1,9 +1,31 @@
 from collections import deque
 
+
+
 def solution(home):
-    answer = 0
+    answer = bfs(home)
     
     return answer
+
+def bfs(home):
+    L = 0
+    check = [True] * 10001
+    check[0] = False
+
+    Q = deque()
+    Q.append(0)
+
+    while Q:
+        n = len(Q)
+        for i in range(n):
+            x = Q.popleft()
+            if x == home:
+                return L
+            for nx in [x-1, x+1, x+5]:
+                if nx >= 0 and nx <= 10000 and check[nx]:
+                    Q.append(nx)
+                    check[nx] = False
+        L += 1
             
 print(solution(10))
 print(solution(14))
